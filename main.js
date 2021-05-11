571c86e50ace8b99330fa57dcdaee985420fb8a6
Luis Lopes <luis.lopes@grupoiv2.local>
2021-05-11 11:53:53 -0300

    teste

--- main.js
+++ main.js
@@ -2,26 +2,22 @@ function changeBorderTl(valor) {
     const conteudo = document.getElementById('content')
 
     conteudo.style.borderTopLeftRadius = `${valor}px`
-    console.log('cheguei')
 }
 
 function changeBorderTr(valor) {
     const conteudo = document.getElementById('content')
 
     conteudo.style.borderTopRightRadius = `${valor}px`
-    console.log('cheguei')
 }
 
 function changeBorderBr(valor) {
     const conteudo = document.getElementById('content')
 
     conteudo.style.borderBottomRightRadius = `${valor}px`
-    console.log('cheguei')
 }
 
 function changeBorderBl(valor) {
     const conteudo = document.getElementById('content')
 
     conteudo.style.borderBottomLeftRadius = `${valor}px`
-    console.log('cheguei')
 }
